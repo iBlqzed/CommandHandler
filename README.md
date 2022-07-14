00dc81dae51047977751f12cebd19a1e5701f938
iBlqzed <iBlqzed@users.noreply.github.com>
2022-07-14 05:06:54 -0500

    Changed README and example
    Changed the readme and the example file

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CommandHandler
 Minecraft Command Handler
+
+* Easy command building
+* Predefine arguments
+* Works with (most) other packs
--- scripts/example.js
+++ scripts/example.js
@@ -1,3 +1,4 @@
+import { Player, world } from "mojang-minecraft";
 import { Command } from "./Papers/commandHandler";
 const kickCommand = new Command({
     name: 'kick',
@@ -6,6 +7,25 @@ const kickCommand = new Command({
 });
 kickCommand.addArgument(0, "playerOnline"); //Adding a playerOnline argument
 kickCommand.callback((player, args) => {
-    player.runCommand(`kick "${args[0].value}" ${args.length > 1 ? args.filter((e, i) => i > 0).map(value => value.value).join(' ') : ''}`); //Kicking the player defined in argument 1, and adding a reason
+    try {
+        player.runCommand(`kick "${args[0].value}" ${args.length > 1 ? args.filter((e, i) => i > 0).map(value => value.value).join(' ') : ''}`);
+    }
+    catch {
+        broadcastMessage('§cYou can not kick the host!', player);
+    } //Kicking the player defined in argument 1, and adding a reason
 });
 kickCommand.register(); //Registering the command
+/**
+ * Broadcast a message (or send it to a player)
+ * @param {string} message Message to broadcast
+ * @param {Player|Player[]} player Player(s) to send the message to
+ * @example broadcastMessage('This message was sent to everyone!')
+ */
+function broadcastMessage(message, player) {
+    if (!player)
+        world.getDimension('overworld').runCommand(`tellraw @a ${JSON.stringify({ rawtext: [{ text: message }] })}`);
+    else if (player instanceof Player)
+        player.runCommand(`tellraw @a ${JSON.stringify({ rawtext: [{ text: message }] })}`);
+    else
+        player.forEach(pL => pL.runCommand(`tellraw @a ${JSON.stringify({ rawtext: [{ text: message }] })}`));
+}
